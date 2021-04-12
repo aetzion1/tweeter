a68dd21c38695c56f9ab1d912981b89dab9c3d84
Adam Etzion <adametzion90@gmail.com>
2021-04-12 09:05:31 -0600

    Update login/session views, update seed file, create spec for login sad and happy paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Posts.destory_all
+User.destroy_all
+
 john = User.create!(
   username: 'johnnyman',
   email: "john.doe@example.com",
